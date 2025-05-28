fef81a200e54be23d76cae24fb77b42302330a91
KaitoKidd86 <liritokun1@gmail.com>
2025-05-28 20:44:02 +0000

    ruta registro

--- src/api/routes.py
+++ src/api/routes.py
@@ -2,11 +2,15 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_bcrypt import Bcrypt
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
 
+
 api = Blueprint('api', __name__)
+bcrypt = Bcrypt()
+
 
 # Allow CORS requests to this API
 CORS(api)
@@ -20,3 +24,45 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/signup', methods=['POST'])
+def signup():
+    data = request.get_json()
+    email = data.get("email")
+    name = data.get("name")
+    password = data.get("password")
+
+    if not email or not password or not name:
+        return jsonify({"msg": "Email and password required"}), 400
+    
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({"msg": "User already exists"}), 409
+    
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(email=email, password=hashed_password,name=name)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({"msg": "User created successfully"}), 201
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"msg": "Email and password required"}), 400
+
+    user = User.query.filter_by(email=email).first()
+    if not user or not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"msg": "Invalid credentials"}), 401
+
+    return jsonify({"msg": "Login successful", "user": user.serialize()}), 200
+
+
+    
