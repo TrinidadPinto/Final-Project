d075f75cd081a113541bb5f3d0f5d67681db68e1
Dolphinh16 <miguelhmercedes@hotmail.com>
2025-05-30 21:24:43 +0000

    Inicio_de_sesiontermida

--- src/front/components/Password.jsx
+++ src/front/components/Password.jsx
@@ -113,3 +113,40 @@ import {useForm} from 'react-hook-form';
 }
 
 export default Password
+// <form className="w-60 mx-auto my-3 justify-content-center col-md-2" onSubmit={handleSubmit(capturaDatos)}>
+
+     //      <div className="form-group mb-3">
+     //      <label htmlhtmlFor="email" className="form-label">Type your e-mail:</label>
+    //     <input type="text" id="email" className={"form-control" + (errors.email ? 'is-invalid' : '')}
+    //       {
+    //       ...register('email', {
+    //         required: 'Your email is required',
+    //         pattern: {
+    //           value: /^[\m-]+@([\m-]+\.)+[\w-] {2,4}$/g,
+    //           message: 'Email invalid'
+    //         }
+    //       })
+    //       }
+    //     />
+    //     <div className="invalid-feedback">
+    //       {errors?.email?.message}
+    //     </div>
+
+    //     <div className="form-group mb-3">
+    //       <label htmlhtmlFor="password" className="form-label">Type your password:</label>
+    //       <input type="password" id="Password" className={"form-control" + (errors.type_password ? 'is-invalid' : '')}
+    //         {
+    //         ...register('type_password', {
+    //           required: 'Password is required!',
+    //           validate: (value) => value === password || 'Your password does not match'
+    //         })
+    //         }
+    //       />
+    //       <div className="invalid-feedback">
+    //         {errors?.type_passwrord?.message}
+    //       </div>
+    //     </div>
+
+    //     <button type="submit" className="btn btn-primary">Submit</button>
+    //   </div>
+    // </form>
