46b27566878f25ae8cbf9d85fa68fc9d8b755096
TrinidadPinto <126416368+TrinidadPinto@users.noreply.github.com>
2025-06-06 17:25:40 +0000

    estilo navbar

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
     const [isValid, setIsValid] = useState(false);
@@ -28,20 +29,23 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className="navbar navbar-expand-md bg-body-tertiary">
+            <nav className="navbar navbar-expand-md" id="navbar">
                 <div className="container">
-                    <Link to={'/'} className="navbar-brand">Resting</Link>
+                    <Link to={'/'} className="navbar-brand">Rest<span className="text-green">ing</span></Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <form className="d-flex flex-column flex-md-row ms-auto" role="search">
                             {isValid ? (
-                                <Link to={`/profile/${localStorage.getItem("user_id")}`} className="btn btn-outline-primary">My Profile</Link>
+                                <>
+                                    <Link to={`/profile/${localStorage.getItem("user_id")}`} className="btn btn-green mx-2">My Profile</Link>
+                                    <button className="btn btn-danger" onClick={() => {localStorage.removeItem("jwt-token"); localStorage.removeItem("user_id"); window.dispatchEvent(new Event("storage")); navigate("/"); }} > Log Out </button>
+                                </>
                             ) : (
                                 <>
-                                    <Link to="/login" className="btn me-md-2 mb-2 mb-md-0">Log In</Link>
-                                    <Link to="/api/signup" className="btn btn-outline-primary">Sign Up</Link>
+                                    <Link to="/login" className="btn btn-light mx-2">Log In</Link>
+                                    <Link to="/api/signup" className="btn btn-green mx-2">Sign Up</Link>
                                 </>
                             )}
                         </form>
